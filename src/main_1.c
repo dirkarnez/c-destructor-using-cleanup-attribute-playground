d74a7131c8d0c3c68866ce141b40d7054e6b05b3
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-11 00:06:59 +0800

    - add src/main_1.c

---
+++ src/main_1.c
@@ -0,0 +1,29 @@
+#define DEFER(type, name, init_func, fini_func_name) \
+    type name __attribute__ ((__cleanup__(fini_func_name))) = init_func
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+void* log_malloc(size_t size) {
+    void* ptr = malloc(size);
+    printf("Allocated %zu bytes: %p\n", size, ptr);
+    return ptr;
+}
+
+void log_free(char** ptr) {
+    free(*ptr);
+    printf("Freed %p\n", *ptr);
+}
+
+int test() {
+    DEFER(char*, a, log_malloc(10), log_free);
+    DEFER(char*, b, log_malloc(20), log_free);
+    DEFER(char*, c, log_malloc(30), log_free);
+    return 0;
+}
+
+int main() {
+    test();
+   return 0;
+}
